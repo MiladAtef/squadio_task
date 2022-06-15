9a357673dff77b022056fada9d4518731865a1fc
miladatef <meladattef2017@gmail.com>
2022-06-15 18:37:44 +0200

    add popular person model

---
+++ lib/models/popular_person_model.dart
@@ -0,0 +1,24 @@
+class PopularPersonModel {
+  final String image;
+  final String name;
+  final String id;
+  final String knownFor;
+  final String gender;
+
+  PopularPersonModel({
+    required this.image,
+    required this.name,
+    required this.id,
+    required this.knownFor,
+    required this.gender,
+  });
+  factory PopularPersonModel.fromJson(json) {
+    return PopularPersonModel(
+      id: json['id'].toString(),
+      image: json['profile_path'] != null ? "https://image.tmdb.org/t/p/original${json['profile_path']}" : "https://images.pexels.com/photos/4089658/pexels-photo-4089658.jpeg?cs=srgb&dl=pexels-victoria-borodinova-4089658.jpg&fm=jpg",
+      name: json['name'] ?? '',
+      knownFor: json['known_for_department'] ?? '',
+      gender: json['gender'] == 2 ? 'Male' : 'Female',
+    );
+  }
+}
